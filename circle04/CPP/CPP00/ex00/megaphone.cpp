cb76d8692b24679f6695e2acdc1ba07c39290506
zackarydevove <zackary.devove@hotmail.com>
2023-04-20 11:02:57 +0200

    modify CP00 ex00

--- circle04/CPP/CPP00/ex00/megaphone.cpp
+++ circle04/CPP/CPP00/ex00/megaphone.cpp
@@ -2,22 +2,13 @@
 
 int	main(int ac, char **av)
 {
-	int		i;
-	int		j;
-
-	i = 0;
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	while (ac > 1 && av && av[i])
-	{
-		j = 0;
-		while (i > 0 && av[i][j])
-        {
+
+	for (int i = 1; i < ac; i++)
+		for (int j = 0; av[i][j]; j++)
 			std::cout << (char)std::toupper(av[i][j]);
-            j++;
-        }
-        i++;
-	}
+			
 	std::cout << std::endl; 
 	return (0);
 }
