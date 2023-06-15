e9f4c0707676505fb82e6dfd1617068fb6217831
zackarydevove <zackary.devove@hotmail.com>
2023-06-15 12:48:47 +0200

    update CPP02

--- circle04/CPP/CPP02/ex02/main.cpp
+++ circle04/CPP/CPP02/ex02/main.cpp
@@ -1,18 +1,57 @@
 #include "Fixed.hpp"
 
 int main( void ) {
-    Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << "======PDF TEST=======" << std::endl;
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
+    Fixed x;
+    Fixed const y( Fixed( 5.05f ) * Fixed( 2 ) );
+
+    std::cout << x << std::endl;
+    std::cout << ++x << std::endl;
+    std::cout << x << std::endl;
+    std::cout << x++ << std::endl;
+    std::cout << x << std::endl;
     
-    std::cout << b << std::endl;
+    std::cout << y << std::endl;
 
-    std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << Fixed::max( x, y ) << std::endl;
+
+    std::cout << "======MY TEST=======" << std::endl;
+
+    Fixed a; // Default constructor
+    Fixed const b(10); // Int constructor
+    Fixed const c(42.42f); // Float constructor
+    Fixed const d(b); // Copy constructor
+
+    a = Fixed(1234.4321f); // Copy assignment operator
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "a - c = " << a - c << std::endl;
+    std::cout << "b * c = " << b * c << std::endl;
+    std::cout << "a / b = " << a / b << std::endl;
+
+    std::cout << "min(a, b) = " << Fixed::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << Fixed::max(a, b) << std::endl;
+
+    std::cout << "a++ is " << a++ << std::endl;
+    std::cout << "a is " << a << std::endl;
+
+    std::cout << "++a is " << ++a << std::endl;
+    std::cout << "a is " << a << std::endl;
+
+    std::cout << "a-- is " << a-- << std::endl;
+    std::cout << "a is " << a << std::endl;
+
+    std::cout << "--a is " << --a << std::endl;
+    std::cout << "a is " << a << std::endl;
 
     return 0;
 }
