3f4879e0cfd66e4b5501f8c4f394a373bc88669e
zackarydevove <zackary.devove@hotmail.com>
2023-06-16 13:28:03 +0200

    update CPP03

--- circle04/CPP/CPP03/ex00/main.cpp
+++ circle04/CPP/CPP03/ex00/main.cpp
@@ -1,10 +1,20 @@
 #include "ClapTrap.hpp"
 
 int main() {
-    ClapTrap claptrap("ClapTrap1");
-    claptrap.attack("Enemy1");
+    ClapTrap claptrap("Bob");
+    claptrap.attack("Tom");
     claptrap.takeDamage(5);
     claptrap.beRepaired(3);
+
+    ClapTrap claptrap2 = claptrap;
+    claptrap2.attack("Job");
+    claptrap2.takeDamage(5);
+    claptrap2.beRepaired(3);
+
+    ClapTrap claptrap3;
+    claptrap3.attack("Dom");
+    claptrap3.takeDamage(5);
+    claptrap3.beRepaired(3);
 
     return 0;
 }
