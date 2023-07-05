16d4eb665d11e2d5f0498c19e0f5840cefb2525e
zackarydevove <zackary.devove@hotmail.com>
2023-07-05 09:28:37 +0200

    update CPP07

--- circle05/CPP/CPP07/ex02/Array.hpp
+++ circle05/CPP/CPP07/ex02/Array.hpp
@@ -5,42 +5,43 @@
 
 template <typename T>
 class Array {
-public:
-    Array() : _array(NULL), _size(0) {}
-    
-    Array(unsigned int n) : _array(new T[n]()), _size(n) {}
-    
-    Array(const Array &other) : _array(new T[other._size]), _size(other._size) {
-        for (unsigned int i = 0; i < _size; i++)
-            _array[i] = other._array[i];
-    }
-    
-    ~Array() { delete[] _array; };
-    
-    Array &operator=(const Array &other) {
-        if (this != &other) {
-            delete[] _array;
-            _array = new T[other._size];
-            _size = other._size;
+    private:
+        T *_array;
+        unsigned int _size;
+    public:
+        Array() : _array(NULL), _size(0) {}
+        
+        Array(unsigned int n) : _array(new T[n]()), _size(n) {}
+        
+        Array(const Array &other) : _array(new T[other._size]), _size(other._size) {
             for (unsigned int i = 0; i < _size; i++)
                 _array[i] = other._array[i];
         }
-        return *this;
-    }
-    
-    T &operator[](unsigned int index) {
-        if (index >= _size)
-            throw std::out_of_range("Array index out of range");
-        return _array[index];
-    }
-    
-    unsigned int size() const {
-        return _size;
-    }
-    
-private:
-    T *_array;
-    unsigned int _size;
+        
+        ~Array() { delete[] _array; };
+        
+        Array &operator=(const Array &other) {
+            if (this != &other) {
+                delete[] _array;
+                _array = new T[other._size];
+                _size = other._size;
+                for (unsigned int i = 0; i < _size; i++)
+                    _array[i] = other._array[i];
+            }
+            return *this;
+        }
+        
+        T &operator[](unsigned int index) {
+            if (index >= _size)
+                throw std::out_of_range("Array index out of range");
+            return _array[index];
+        }
+
+        T   *getArray() { return _array; };
+        
+        unsigned int size() const {
+            return _size;
+        }
 };
 
 #endif
--- circle05/CPP/CPP07/ex02/main.cpp
+++ circle05/CPP/CPP07/ex02/main.cpp
@@ -3,16 +3,31 @@
 
 int main() {
     try {
+        // Try default constructor
+        Array<int> null;
+        if (null.getArray() == NULL)
+            std::cout << "default constructor array_ = NULL" << ' ';
+        std::cout << '\n';
+
+
+        // Try unsigned n constructor
         Array<int> arr(10);
         for (unsigned int i = 0; i < arr.size(); ++i) {
-            arr[i] = i * 10;
+            arr[i] = i;
             std::cout << arr[i] << ' ';
         }
         std::cout << '\n';
 
+        // Try copy constructor
         Array<int> arr2(arr);
         for (unsigned int i = 0; i < arr2.size(); ++i)
             std::cout << arr2[i] << ' ';
+        std::cout << '\n';
+
+        // Try = operator
+        Array<int> arr3 = arr;
+        for (unsigned int i = 0; i < arr3.size(); ++i)
+            std::cout << arr3[i] << ' ';
         std::cout << '\n';
         
         // Trigger out of range exception
