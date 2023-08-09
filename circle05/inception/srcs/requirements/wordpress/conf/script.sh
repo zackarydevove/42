b5a9bce31be600d83fb2965e5ecbe853d6c387d4
zackarydevove <zackary.devove@hotmail.com>
2023-08-09 11:09:13 +0200

    inception working but not clean

--- circle05/inception/srcs/requirements/wordpress/conf/script.sh
+++ circle05/inception/srcs/requirements/wordpress/conf/script.sh
@@ -4,30 +4,43 @@
 if [ -f /wordpress_installed ]
 then
 	echo "wordpress already installed"
+	sleep 10
 else
 	echo "Configuring wordpress ..."
 
+	# Wait for the database to be created correctly
+	sleep 30
+
 	# Create the wp-config.php file
-	wp config create --allow-root --dbname="$MYSQL_DATABASE" \
-					--dbuser="$MYSQL_USER" \
-					--dbpass="$MYSQL_PASSWORD" \
-        			--dbhost="$MYSQL_HOST" \
-					--path="$WP_PATH";
+	# wp config create --allow-root \
+	# 				--dbname="$MYSQL_DATABASE" \
+	# 				--dbuser="$MYSQL_USER" \
+	# 				--dbpass="$MYSQL_PASSWORD" \
+    #     			--dbhost="$MYSQL_HOST" \
+	# 				--path="$WP_PATH";
+
+	# Setup the wp-config.php file
+	sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wordpress/wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wordpress/wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOST/g" /var/www/html/wordpress/wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/html/wordpress/wp-config-sample.php
+	mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
 	# Fill wordpress first page
 	wp core install --allow-root \
-					--url="$DOMAIN_NAME" \
-					--title="$WP_TITLE" \
-					--admin_user="$WP_ADMIN_LOGIN" \
-					--admin_password="$WP_ADMIN_PASSWORD" \
-					--admin_email="$WP_ADMIN_EMAIL" \
-					--path="$WP_PATH";
+					--url=${DOMAIN_NAME} \
+					--title=${WP_TITLE} \
+					--admin_user=${WP_ADMIN_LOGIN} \
+					--admin_password=${WP_ADMIN_PASSWORD} \
+					--admin_email=${WP_ADMIN_EMAIL} \
+					--path=${WP_PATH};
 
 	# Add user1
 	wp user create --allow-root \
-					"$WP_USER1_LOGIN" "$WP_USER1_EMAIL" \
-					--user_pass="$WP_USER1_PASSWORD" \
-					--path="$WP_PATH";
+					${WP_USER1_LOGIN} \
+					${WP_USER1_EMAIL} \
+					--user_pass=${WP_USER1_PASSWORD} \
+					--path=${WP_PATH};
 
 	touch /wordpress_installed
 fi
